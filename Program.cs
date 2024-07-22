15c3ec9f7084ad0873e3ead4c4cbf2b4be070307
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-07-22 22:19:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace UsernameConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Enter your username:");
+            string username = Console.ReadLine();
+            string filePath = "D:\\aproSCM\\Usernames.txt"; //file path
+
+            // Append u_name;
+            File.AppendAllText(filePath, username + "\n"); // this directly appends any word to the file
+            Console.WriteLine("Username added successfulllyy.");
+           
+        }
+    }
+}
